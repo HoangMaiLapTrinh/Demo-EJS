1e2126b30dfffe8897705b436c578e5154704f67
HoangMaiLapTrinh <hoangmai020603@gmail.com>
2025-03-01 19:22:14 +0700

    Update 3
    MongoDB connection

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
 
 app.use(express.static(path.join(__dirname, "public")));
+
+// MongoDB Connection
+mongoose.connect('mongodb+srv://hoangmai020603:mldQUI1udP6GuV7D@hoangmai.al06t.mongodb.net/DATABASE?retryWrites=true&w=majority')
+.then(() => console.log('✅ Connected to MongoDB Atlas'))
+.catch(error => console.error('❌ Could not connect to MongoDB Atlas:', error));
 
 // HTTP Logger
 app.use(morgan('combined'));
