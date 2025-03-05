a1e08df728fab239044efb29e710445f9cbb95ec
HoangMaiLapTrinh <hoangmai020603@gmail.com>
2025-03-05 20:00:29 +0700

    update 4
    modify header and add folder js
    connection mongodb

--- src/index.js
+++ src/index.js
@@ -4,15 +4,34 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
-const mongoose = require('mongoose');
-
-
 app.use(express.static(path.join(__dirname, "public")));
 
 // MongoDB Connection
-mongoose.connect('mongodb+srv://hoangmai020603:mldQUI1udP6GuV7D@hoangmai.al06t.mongodb.net/DATABASE?retryWrites=true&w=majority')
-.then(() => console.log('✅ Connected to MongoDB Atlas'))
-.catch(error => console.error('❌ Could not connect to MongoDB Atlas:', error));
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://hoangmai020603:0971172603@hoangmai.al06t.mongodb.net/?retryWrites=true&w=majority&appName=HoangMai";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // HTTP Logger
 app.use(morgan('combined'));
